3b715d52c6817f1e6c30f9073a470e7a1e6a164c
Matt-Chee123 <cheematthew10@gmail.com>
2026-05-07 13:17:16 +0100

    add in redis for feature processor

--- consumers/feature_processor/main.py
+++ consumers/feature_processor/main.py
@@ -2,15 +2,20 @@ import os
 from shared.kafka_producer import KafkaProducerWrapper
 from shared.windows import SymbolFeatures
 from shared.kafka_client import KafkaConsumerWrapper
-
+import redis
+import json
 
 WINDOW_LENGTHS_MS = [60_000, 300_000]
 INPUT_TOPIC = "market.trades"
 OUTPUT_TOPIC = "market.features"
+SCHEMA_VERSION = "v1"
 
 
 def main():
     KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
+    REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
     consumer = KafkaConsumerWrapper(
         broker=KAFKA_BROKER,
@@ -32,6 +37,9 @@ def main():
             )
             for record in features.snapshot(trade["symbol"], trade["timestamp"]):
                 producer.send(OUTPUT_TOPIC, key=record["symbol"], message=record)
+                key = f"feat:{record['symbol']}:{record['window_seconds']}s:{SCHEMA_VERSION}"
+                print(key, record)
+                r.set(key, json.dumps(record, default=str))
 
             count += 1
             if count % 1000 == 0:
@@ -44,7 +52,7 @@ def main():
         producer.flush()
         consumer.commit()
         producer.close()
-
+        r.close()
 
 if __name__ == "__main__":
     main()
